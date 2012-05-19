495fa6a092748e925ca8018a1f145191b28107b3
Thomas Dohmke <thomas@dohmke.de>
2012-05-19 21:59:32 +0200

    Removed missing comment file

--- lib/ticketmaster-bugshelf.rb
+++ lib/ticketmaster-bugshelf.rb
@@ -1,5 +1,5 @@
 require File.dirname(__FILE__) + '/bugshelf/bugshelf-api.rb'
 
-%w{ bugshelf ticket project comment }.each do |f|
+%w{ bugshelf ticket project }.each do |f|
   require File.dirname(__FILE__) + '/provider/' + f + '.rb';
 end
